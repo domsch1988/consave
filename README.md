9330558ecfcda577eaba4886019d6d5bcdc9dfe2
Dominik Schlack <dominik@schlack.net>
2017-12-21 18:59:39 +0100

    Initial Sync 2

---
+++ README.md
@@ -0,0 +1,1 @@
+# consave
---
+++ save.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+mkdir -p ./dots
+
+# i3 Configuration
+cp ~/.config/i3/config ./dots/i3config
+
+# i3Bar Configuration
+
+# i3status Configuration
+
+# i3blocks Configuration
+cp ~/.config/i3blocks/config ./dots/i3blocksconfig
+
+# .Xresources
+
+# Terminator Configuration
+cp ~/.config/terminator/config ./dots/terminatorconfig
+
