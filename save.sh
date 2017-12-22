8498a9cbad9dea1ce68207b43d1944fea9016efe
Dominik Schlack <dominik@schlack.net>
2017-12-22 13:03:19 +0100

    Save Testing

--- save.sh
+++ save.sh
@@ -18,3 +18,5 @@ cp ~/.config/terminator/config ./dots/terminatorconfig
 
 # fish Configuration
 cp ~/.config/fish/config.fish ./dots/fishconfig
+
+# Fonts copying and Additional fish stuff will go here
