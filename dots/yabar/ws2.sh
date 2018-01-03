7f25588a3784e6600de4f18118d3d1d88929ac3d
Dominik Schlack <dominik.schlack@gmail.com>
2018-01-03 07:52:02 +0100

    zsh-test

--- dots/yabar/ws2.sh
+++ dots/yabar/ws2.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-i3-msg -t get_workspaces | jq -j '.[] | select(.rect.x | contains(0)) | if .visible == true then "<span underline=\"double\">"+" "+.name+"   "+"</span>" else " "+.name+"  " end'
+i3-msg -t get_workspaces | jq -j '.[] | select(.rect.x | contains(0)) | if .visible == true then "<span underline=\"double\">"+" "+.name+" "+"</span>" else " "+.name+" " end'
 
