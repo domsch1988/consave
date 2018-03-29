a357f4f64c540bfae3f2888c3bca1c1792e2f024
domsch1988 <dominik@schlack.net>
2018-03-29 08:52:46 +0200

    Update wpgtkmenu.sh

--- full/openbox/wpgtkmenu.sh
+++ full/openbox/wpgtkmenu.sh
@@ -5,7 +5,7 @@ echo "<openbox_pipe_menu>"
 for entry in $entrys
 do
 	name=${entry%.*}
-	echo "<item icon=\"/home/dschlack/.wallpapers/sample/$entry.sample.png\" label=\"$name\">"
+	echo "<item icon=\"/home/dschlack/.config/wpg/wallpapers/sample/$entry.sample.png\" label=\"$name\">"
 	echo "<action name=\"Execute\">"
 	echo "<execute>wpg -s $entry</execute>"
 	echo "</action>"
